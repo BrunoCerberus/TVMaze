b4451dbda0a21ff7e1f15d463ab8cfc5b22bae71
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-06-24 18:19:02 -0300

    Add some colors and snapshot testing lib

---
+++ TVMaze/Core/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  TVMaze
+//
+//  Created by bruno on 24/06/23.
+//
+
+import SwiftUI
+
+// MARK: - Text
+extension Color {
+    static let white: Color = Color(hex: "FFFFFF")
+    static let darkGray: Color = Color(hex: "2B2B2B")
+    static let lightGray: Color = Color(hex: "424242")
+    static let black: Color = Color(hex: "000000")
+    static let text1: Color = Color(hex: "888888")
+}
