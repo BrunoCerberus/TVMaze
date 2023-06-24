ddb7da6eadf77f8bfdebe87ed1f6615b51d166b8
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-06-24 17:11:26 -0300

    first commit

---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by bruno on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
