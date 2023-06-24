84cebd314b4e3af42d54e43fcc4a05331646cdd5
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-06-24 18:28:32 -0300

    Add Layout with default padding and its multiplier

---
+++ TVMaze/Core/Layout.swift
@@ -0,0 +1,14 @@
+//
+//  Layout.swift
+//  TVMaze
+//
+//  Created by bruno on 24/06/23.
+//
+
+import Foundation
+
+struct Layout {
+    static func padding(_ multiplier: CGFloat) -> CGFloat {
+        multiplier * 8
+    }
+}
